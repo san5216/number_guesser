86ae008a606b5c300d6ed45792bbac5afbbe323c
Shawn Norris <mster.norris@gmail.com>
2025-07-19 10:18:34 -0500

    Finish refactor of Game

--- main.py
+++ main.py
@@ -42,59 +42,83 @@ class Game:
                 print("Please choose a valid difficulty level: 1, 2, or 3.")
 
 
+    def get_valid_guess(self):
+        while True:
+            try:
+                guess = int(input("Enter your guess: "))
+                if self.MIN_NUMBER <= guess <= self.MAX_NUMBER:
+                    return guess
+                else:
+                    print("Please enter a number between 1 and 100.")
+            except ValueError:
+                print("Please enter a number between 1 and 100.")
+
+    def display_hint(self, guess):
+
+        if guess > self.secret_number:
+            print(f"\nIncorrect! The number is less than {guess}")
+        else:
+            print(f"\nIncorrect! The number is greater than {guess}")
+
+
+    def display_remaining_guesses(self):
+
+        remaining = self.total_guesses - self.guesses_made
+        if remaining > 0:
+            print(f"\nYou have {remaining} guess{"es" if remaining != 1 else ""} remaining.\n")
+
+    def play_round(self):
+        self.secret_number = randint(self.MIN_NUMBER, self.MAX_NUMBER+1)
+        self.guesses_made = 0
+        self.total_guesses = self.difficulty_level.value["guesses"]
+
+        print(f"You have {self.total_guesses} chances to guess the number.", end="\n\n")
+
+        while self.guesses_made < self.total_guesses:
+
+            guess = self.get_valid_guess()
+            self.guesses_made += 1
+
+            if guess == self.secret_number:
+                print(f"You guessed the secret number in {self.guesses_made} guesses!")
+                return True
+
+            self.display_hint(guess)
+
+            if self.guesses_made < self.total_guesses:
+                self.display_remaining_guesses()
+
+        print(f"\nYou are out of guesses! The secret number was {self.secret_number}.")
+        return False
+
+    def play_again(self):
+
+        while True:
+            play_again = input("\nDo you want to play again? (y/n) ").lower().strip()
+
+            if play_again in ["y", "yes"]:
+                return True
+            elif play_again in ["n", "no"]:
+                return False
+            else:
+                print("Please enter 'y' for yes or 'n' for no.")
+
+
     def run(self):
         self.display_welcome()
         self.difficulty_level = self.get_difficulty_level()
+
+        while True:
+            self.play_round()
+
+            if not self.play_again():
+                break
 
 
 def main():
 
     game = Game()
     game.run()
-
-    # total_guesses = get_difficulty_level()
-    # print()
-    # print(f"You have {total_guesses} chances to guess the number.", end="\n\n")
-    #
-    # play_again = ""
-    # while play_again != "n":
-    #     answer = randint(1, 100)
-    #     play_game(total_guesses, answer)
-    #     play_again = input("\nDo you want to play again? (y/n) ").lower()
-    #
-    # print("Thanks for playing!")
-
-def play_game(total_guesses, secret_number):
-
-    guesses_made = 1
-    while guesses_made <= total_guesses:
-
-        valid = False
-        while not valid:
-            try:
-                guess = int(input("Enter your guess: "))
-                if guess in range(1, 101):
-                    valid = True
-                else:
-                    raise ValueError
-            except ValueError:
-                print("Please enter a number between 1 and 100.")
-
-        if guess > secret_number:
-            print(f"\nIncorrect! The number is less than {guess}")
-        elif guess < secret_number:
-            print(f"\nIncorrect! The number is greater than {guess}")
-        else:
-            print(f"You guessed the secret number in {guesses_made} guesses!")
-            return
-
-        remaining = total_guesses - guesses_made
-        if remaining > 0:
-            print(f"\nYou have {total_guesses - guesses_made} guesses remaining.")
-        else:
-            print(f"\nYou are out of guesses! The secret number was {secret_number}.")
-
-        guesses_made += 1
 
 
 if __name__ == '__main__':
